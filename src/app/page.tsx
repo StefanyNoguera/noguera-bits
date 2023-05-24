d17055f6ce4e5abf7608fb1182bae4f456578908
Stefany Noguera <stefanynoguerar@gmail.com>
2023-05-24 11:49:36 -0500

    image is now touching the bottom of the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,36 +3,33 @@ import {AiOutlineCaretDown} from "react-icons/ai";
 
 export default function Home() {
   return (
-    <main className="bg-gradient-to-b from-white to-gray h-screen">
+    <main className="bg-gradient-to-b from-white to-gray min-h-screen">
 
-      <div className="mx-10">
-        <section>
-          <nav className="flex justify-between pt-7">
-            <div className="bg-stone-800 text-stone-100 p-2">
-              <h1>NOGUERA.BITS</h1>
-            </div>
+      <div className="mx-10 h-full flex flex-col justify-between">
 
-            <div className="flex p-2">
-              <h1 className="text-stone-800">CONTACT</h1>
-              <p className="pt-1"><AiOutlineCaretDown/></p>
-            </div>
+        <nav className="flex justify-between pt-7">
+          <div className="bg-stone-800 text-stone-100 p-2">
+            <h1>NOGUERA.BITS</h1>
+          </div>
 
-          </nav>
-        </section>
+          <div className="flex p-2">
+            <h1 className="text-stone-800">CONTACT</h1>
+            <p className="pt-1"><AiOutlineCaretDown/></p>
+          </div>
+        </nav>
 
-      <section>
-        <div className="pt-4 flex justify-center items-center flex-grow">
-          <Image
-            src="/casa.png"
-            fill="inherit"
-              
-            alt="Picture of the author"
-          />
+        <div className="flex-grow">
+          <div className="absolute inset-x-0 bottom-0 flex justify-center h-80 min-w-full md:h-96 md:min-w-full">
+            <Image
+              src="/casa.png"
+              fill= "relative"
+              style={{ objectFit: 'cover', objectPosition: 'left top'}}
+              alt="Picture of the author"
+            />
+          </div>
         </div>
-      </section>
-
 
       </div>
     </main>
-  )
+  );
 }
